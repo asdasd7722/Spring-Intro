f866142fdc2d493bdccc2a453ce39236338ab1e2
Sangjun Park <psjune9707@naver.com>
2024-11-08 00:22:00 +0900

    lecture 4: 회원 리포지토리 테스트 케이스 작성

---
+++ src/main/java/Spring/Intro/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package Spring.Intro.repository;
+
+import Spring.Intro.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
